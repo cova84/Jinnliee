daf28204a01ad05f851c5efb4e56f5f834e0c2fb
小林由知 <kobayashiyoshitomo@USER-no-MacBook-Pro.local>
2017-11-15 12:23:59 +0800

    start_UI_fix

---
+++ Jinnliee/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Jinnliee
+//
+//  Created by 小林由知 on 2017/11/15.
+//  Copyright © 2017年 Yoshitomo Kobatashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
